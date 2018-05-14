723443c077db418e9b86a8b3b356f92ec8388bbe
Jonathan Hogg <me@jonathanhogg.com>
2018-05-14 12:21:43 +0100

    Radical redesign

---
+++ index.js
@@ -0,0 +1,77 @@
+
+const ScrollPeriod = 300,
+      ScrollWait = 200,
+      ScrollInterval = 20,
+      ScrollUp = document.getElementById('scroll-up'),
+      ScrollDown = document.getElementById('scroll-down');
+
+var ignoreScroll = false,
+    forceScroll = false,
+    scrollStartTime,
+    scrollStartPosition,
+    scrollDestination,
+    scrollTimeout = undefined;
+
+function update()
+{
+    var pageHeight = window.innerHeight,
+        scrollTop = document.body.scrollTop,
+        offset = scrollTop % pageHeight;
+    document.body.style.height = pageHeight.toString() + "px";
+    if (offset > pageHeight/2)
+        offset -= pageHeight;
+    if (!forceScroll && !ignoreScroll)
+    {
+        scrollStartTime = performance.now() + ScrollWait;
+        scrollStartPosition = scrollTop;
+        scrollDestination = scrollStartPosition - offset;
+        if (scrollTimeout != undefined)
+            clearTimeout(scrollTimeout);
+        scrollTimeout = setTimeout(scroller, ScrollWait);
+    }
+    ignoreScroll = false;
+    var opacity = Math.max(0, 0.5-Math.abs(2*offset/pageHeight));
+    ScrollUp.style.opacity = opacity;
+    ScrollUp.style.display = scrollTop > pageHeight/2 ? 'block' : 'none';
+    ScrollDown.style.opacity = opacity;
+    ScrollDown.style.display = scrollTop < document.body.scrollHeight-pageHeight*1.5 ? 'block' : 'none';
+}
+
+update();
+
+function scroller()
+{
+    scrollTimeout = undefined;
+    var now = performance.now();
+    if (now > scrollStartTime)
+    {
+        var t = Math.min(1, (now-scrollStartTime)/ScrollPeriod),
+            position = (scrollDestination-scrollStartPosition)*(1-Math.cos(t*Math.PI))/2 + scrollStartPosition;
+        ignoreScroll = true;
+        window.scrollTo(0, position);
+    }
+    if (now < scrollStartTime+ScrollPeriod)
+        scrollTimeout = setTimeout(scroller, ScrollInterval);
+    else
+        forceScroll = false;
+}
+
+window.addEventListener('scroll', update);
+window.addEventListener('resize', update);
+
+ScrollUp.onclick = function() {
+    scrollStartTime = performance.now();
+    scrollStartPosition = document.body.scrollTop;
+    scrollDestination = Math.trunc(document.body.scrollTop/document.body.clientHeight - 1)*document.body.clientHeight;
+    forceScroll = true;
+    scroller();
+};
+
+ScrollDown.onclick = function() {
+    scrollStartTime = performance.now();
+    scrollStartPosition = document.body.scrollTop;
+    scrollDestination = Math.trunc(document.body.scrollTop/document.body.clientHeight + 1)*document.body.clientHeight;
+    forceScroll = true;
+    scroller();
+};
+
